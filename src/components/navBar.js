ba3328f41cf470b845b3a9c638d8dca46cf1abf0
lisandroseia <lisandroseia11@gmail.com>
2022-02-15 15:10:38 -0300

    Fix modules

---
+++ src/components/navBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+
+  return (
+    <nav className="navBar">
+      <ul>
+        {links.map((link) => {
+          return <li key={link.id}>{link.text}</li>;
+        })}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
